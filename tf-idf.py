b789b790962a648d6e2709ecb8a2337fd1dd1c4f
Adi Arbili <adiarbili@gmail.com>
2019-01-03 23:19:35 +0200

    few changes, non-critic

--- tf-idf.py
+++ tf-idf.py
@@ -6,8 +6,8 @@ import re
 # TFtd - Number of times that term t occurs in document d.
 # DFt - Number of documents that contain term t.
 
-IDFt = lambda N, DFt: math.log10(N / DFt) if DFt != 0 else 0
-one_plus_log_TFdt = lambda TFdt: 1 + math.log10(TFdt) if TFdt != 0 else 0
+IDFt = lambda N, DFt: math.log10(N / DFt) if DFt != 0 else 0.0
+one_plus_log_TFdt = lambda TFdt: 1 + math.log10(TFdt) if TFdt != 0 else 0.0
 
 
 def read_file(full_path):
@@ -22,7 +22,7 @@ def read_file(full_path):
     return list
 
 
-def calc_TF_IDF(docs, terms):
+def TF_IDF(docs, terms):
     TF_IDF = []
 
     TF_list = TF(docs, terms)
@@ -37,7 +37,7 @@ def calc_TF_IDF(docs, terms):
         TF_IDF_row = []
         log_IDF = log_IDF_list[i]
         for value in TF_row:
-            TF_IDF_row.append(log_IDF * value)
+            TF_IDF_row.append(round(log_IDF * value, 3))
 
         i += 1
         TF_IDF.append(TF_IDF_row)
@@ -46,7 +46,7 @@ def calc_TF_IDF(docs, terms):
 
 
 def TF(docs, terms):
-    TF_res = []
+    TF_list = []
 
     for term in terms:
         TF_term = []
@@ -55,9 +55,9 @@ def TF(docs, terms):
         for doc in docs:
             TF_term.append(TFtd(doc, reg_term))
 
-        TF_res.append(TF_term)
+        TF_list.append(TF_term)
 
-    return TF_res
+    return TF_list
 
 
 def TFtd(doc, reg_term):
@@ -69,7 +69,7 @@ def TFtd(doc, reg_term):
 
 
 def calc_DFt(TF):
-    DF = []
+    DF_list = []
 
     for TFdt in TF:
 
@@ -79,18 +79,18 @@ def calc_DFt(TF):
             if val >= 1:
                 DFt += 1
 
-        DF.append(DFt)
+        DF_list.append(DFt)
 
-    return DF
+    return DF_list
 
 
 def calc_log_IDF(DF_list):
-    res = []
+    log_IDF_list = []
 
     for DFt in DF_list:
-        res.append(IDFt(number_of_doc, DFt))
+        log_IDF_list.append(round(IDFt(number_of_doc, DFt), 3))
 
-    return res
+    return log_IDF_list
 
 
 def calc_one_plus_log_TF(TF_list):
@@ -99,27 +99,27 @@ def calc_one_plus_log_TF(TF_list):
     for TFdt in TF_list:
         TF_row = []
         for val in TFdt:
-            TF_row.append(one_plus_log_TFdt(val))
+            TF_row.append(round(one_plus_log_TFdt(val), 3))
 
         res.append(TF_row)
 
     return res
 
 
-documents = read_file("document.txt")
+documents = read_file("documents.txt")
 terms = read_file("terms.txt")
 # documents = read_file("docs_hw1.txt")
 # terms = read_file("terms_hw1.txt")
 
 number_of_doc = len(documents)
 
-TF_list, DF_list, log_IDF_list, one_plus_log_TF_list, TF_IDF = calc_TF_IDF(documents, terms)
+TF_list, DF_list, log_IDF_list, one_plus_log_TF_list, TF_IDF = TF_IDF(documents, terms)
 
 print("TF table:")
 print(*TF_list, sep="\n", end="\n\n")
 
 print("DF table:")
-print(*DF_list, sep="\n", end="\n\n")
+print(DF_list, end="\n\n")
 
 print("log IDF table:")
 print(log_IDF_list, end="\n\n")
